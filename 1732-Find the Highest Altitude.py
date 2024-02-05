fcb9cf055da6742d9b8df57a8a516ad6fdf2924a
Anusha Upadhyay <134798865+jfnqnq@users.noreply.github.com>
2024-02-05 19:56:23 +0530

    Add files via upload

---
+++ 1732-Find the Highest Altitude.py
@@ -0,0 +1,8 @@
+#1732-Find the Highest Altitude
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        ans = h = 0
+        for v in gain:
+            h += v
+            ans = max(ans, h)
+        return ans
